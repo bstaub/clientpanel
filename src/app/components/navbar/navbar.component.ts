f2892a008963ca55bc18c3224769472a776f6729
Bruno Staub <bruno.staub@gmail.com>
2018-09-07 23:35:26 +0200

    Logout and Navbar Links depending on Auth Status modified

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Client} from '../../models/Client';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) {
+  }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('Sie haben sich beim System erfolgreich ausgeloggt', {
+      cssClass: 'alert-success', timeout: 4000
+    });
+    this.router.navigate(['/login']);
   }
 
 }
