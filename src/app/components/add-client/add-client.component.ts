af91f7fce68d05f8a2708d66788588be4906bf00
Bruno Staub <bruno.staub@gmail.com>
2018-09-07 11:15:03 +0200

    Client Panel mit Bootstrap4 Navbar mit Router (Start 56. - 59. Routing Module Setup)

---
+++ src/app/components/add-client/add-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-add-client',
+  templateUrl: './add-client.component.html',
+  styleUrls: ['./add-client.component.scss']
+})
+export class AddClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/client-details/client-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-client-details',
+  templateUrl: './client-details.component.html',
+  styleUrls: ['./client-details.component.scss']
+})
+export class ClientDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
