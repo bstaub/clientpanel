067f147a377dae55a145b87732a10d176c47ed8a
Bruno Staub <bruno.staub@gmail.com>
2018-09-07 12:50:21 +0200

    Clients 2 Dashboad *ngFor with Bootstrap4 mit currency Pipe and add Total with reduce function, anstatt for loop

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,6 +9,7 @@ import {Client} from '../../models/Client';
 })
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwned: number;
 
   constructor(private clientService: ClientService) { }
 
@@ -16,7 +17,14 @@ export class ClientsComponent implements OnInit {
     this.clientService.getClients().subscribe(clients => {
       console.log(clients);
       this.clients = clients;
+      this.getTotalOwned();
     });
+  }
+
+  getTotalOwned() {
+    this.totalOwned = this.clients.reduce((total, client) => {
+      return total + client.balance;
+    }, 0);
   }
 
 }
