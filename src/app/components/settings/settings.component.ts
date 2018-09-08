b84a7b9a9773ae40afbbd4fc80f59c65e842da55
Bruno Staub <bruno.staub@gmail.com>
2018-09-08 11:17:55 +0200

    Adding Settings Page and Deploy to Firebase. App Finished :-)

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {SettingsService} from '../../services/settings.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Settings} from '../../models/Settings';
 
 @Component({
   selector: 'bs-settings',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  public settings: Settings;
 
-  constructor() { }
+  constructor(
+    private settingsService: SettingsService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+    this.settings = this.settingsService.getSettings();
+  }
+
+  onSubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.flashMessage.show('Einstellungen gespeichert', {
+      cssClass: 'alert-success', timeout: 4000
+    });
   }
 
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -10,10 +10,17 @@ export class SettingsService {
     disableBalanceOnAdd: false,
     disableBalanceOnEdit: false
   }
-  constructor() { }
+  constructor() {
+    if(localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));  // string nach object umwandeln JSON.parse!
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
   }
 
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));  // object nach String umwandeln JSON.stringify!
+  }
 }
